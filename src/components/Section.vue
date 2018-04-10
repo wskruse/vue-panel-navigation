c1cdc0a9998c5f4880b2941c5a00144142531715
Will Kruse <wskruse@gmail.com>
2018-04-10 17:07:04 -0400

    Well that was annoying - apparently this doesn't work anymore

--- src/components/Section.vue
+++ src/components/Section.vue
@@ -32,25 +32,12 @@
             }
         },
         mounted() {
-            let vm = this;
             this.$el.dataset.uuid = this.$shortId();
             this.$el.dataset.title = this.title;
             this.section = this.$addSection(this.title, this.$el);
-            let baseOffset = scrollmonitor.viewportHeight / 3;
-            let watcher = this.watcher = scrollmonitor.create(
-                this.$el,
-                {
-                    top: this.offsetTop,
-                    bottom: this.offsetBottom
-                }
-            );
-            watcher.fullyEnterViewport(function () {
-                vm.$setActiveSection(this.$el.dataset.uuid);
-            });
             scrollmonitor.update();
         },
         destroyed() {
-            this.watcher.destroy();
         }
     };
 </script>
